e6db2d7b03ed40e514ce68b4dc233a7c7461fe29
Brandon-Harris <brandonharris177@gmail.com>
2019-07-23 17:38:08 -0600

    Finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -46,19 +46,40 @@ last(items, function(last){
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb = (x + y);
+  return cb(x, y)
 }
 
-console.log(sumNums(2, 6));
+function cb(x, y){
+  return x + y;
+}
+
+console.log(sumNums(0, 0, cb));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y)
 }
+
+function cb(x, y){
+  return x * y;
+}
+
+console.log(multiplyNums(0, 0, cb))
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list);
 }
+
+function cb(item, list){
+  for ( let i = 0; i < list.length; i++){
+    if (list[i] === item) {return true;}
+  }
+  return false;
+}
+
+console.log(contains("Gum", items, cb));
 
 /* STRETCH PROBLEM */
 
